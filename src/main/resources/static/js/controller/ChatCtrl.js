22f55d2eb35b9a8ca0da832af250058c46cc77a4
Michael Holopainen <michael.holopainen@solita.fi>
2014-09-23 17:28:39 +0300

    started changing chat to use global long polling and listen on messages

--- src/main/resources/static/js/controller/ChatCtrl.js
+++ src/main/resources/static/js/controller/ChatCtrl.js
@@ -1,53 +1,63 @@
 'use strict';
 
 sevenMonitor
-.controller('ChatCtrl',  function($scope, $q, $http, $timeout,  $location, $anchorScroll, SettingsService, TelnetService) {
+.controller('ChatCtrl',  function($scope, $q, $http, $timeout, $document, $location, $anchorScroll, SettingsService, TelnetService) {
    $scope.chatLog = [];
    $scope.processing = false;
    $scope.loading = true;
 
-   var readChat = function() {
-        $scope.processing = true;
-        var lastLine;
-        if ($scope.chatLog && $scope.chatLog.lenght > 0) {
-             lastLine = $scope.chatLog[$scope.chatLog.lenght-1];
-        }
-        $timeout(function() {
-             $scope.$emit('show_loading', '');
-             TelnetService.chat(lastLine)
-                .success(function (data) {
-                   $scope.chatLog = data;
+    var readChat = function() {
+      $scope.processing = true;
+      var lastLine;
+      if ($scope.chatLog && $scope.chatLog.lenght > 0) {
+        lastLine = $scope.chatLog[$scope.chatLog.lenght - 1];
+      }
+      $timeout(function() {
+        $scope.$emit('show_loading', '');
+        TelnetService.chat(lastLine)
+          .success(function(data) {
+            $scope.chatLog = data;
 
-                   $anchorScroll();
-                })
-                .error(function (status) {
-                    $scope.$broadcast('status_error', 'Reading chat failed, error ' + status);
-                    return false;
-                });
-                 $scope.loading = false;
-                 $scope.$emit('hide_loading', '');
-               readChat();
-           }, 1000);
-         $scope.processing = false;
-   }
+            // $anchorScroll();
+          })
+          .error(function(status) {
+            $scope.$broadcast('status_error', 'Reading chat failed, error ' + status);
+            return false;
+          });
+        $scope.loading = false;
+        $scope.$emit('hide_loading', '');
+        readChat();
+      }, 1000);
+      $scope.processing = false;
+    };
+
    readChat();
 
    $scope.message;
    $scope.messageAs;
    $scope.useAs;
-   $scope.send = function() {
-       console.log('sending message ' +$scope.message );
-       var msg = $scope.message;
-       if ($scope.useAs) {
-           msg = '[' + $scope.messageAs + '] ' +  $scope.message;
-       }
-       TelnetService.say(msg)
-           .success(function (data) {
-              $scope.status = data;
-              $scope.message = undefined;
-           })
-           .error(function (status) {
-               alert(status);
-           });
-   }
+    $scope.send = function() {
+      console.log('sending message ' + $scope.message);
+      var msg = $scope.message;
+      if ($scope.useAs) {
+        msg = '[' + $scope.messageAs + '] ' + $scope.message;
+      }
+      TelnetService.say(msg)
+        .success(function(data) {
+          $scope.status = data;
+          $scope.message = undefined;
+        })
+        .error(function(status) {
+          alert(status);
+        });
+    };
+
+    $scope.$on('CHAT', function (event, message) {
+      console.log('on chat  ' + JSON.stringify(message));
+      //for (var i in message.data) {
+        $scope.chatLog += message.data;
+      //}
+      $document.getElementById('lastMessage' ).scrollIntoView();
+    });
+
 });
